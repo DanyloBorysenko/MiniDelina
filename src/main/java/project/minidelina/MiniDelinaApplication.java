6cba6064e6060f390b71678bd6b5abe046cd8e85
Danylo Borysenko <borysenkodanyl@gmail.com>
2023-06-17 19:19:16 +0300

    first commit

---
+++ src/main/java/project/minidelina/MiniDelinaApplication.java
@@ -0,0 +1,13 @@
+package project.minidelina;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniDelinaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiniDelinaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/project/minidelina/MiniDelinaApplicationTests.java
@@ -0,0 +1,13 @@
+package project.minidelina;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniDelinaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
