c4a19281d0fa107d48ed46be689bcc26366041fe
Danylo Borysenko <borysenkodanyl@gmail.com>
2023-06-17 20:33:32 +0300

    indexcontroller was created

--- src/test/java/project/minidelina/TestMiniDelinaApplication.java
+++ src/test/java/project/minidelina/TestMiniDelinaApplication.java
@@ -2,18 +2,10 @@ package project.minidelina;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.TestConfiguration;
-import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
-import org.springframework.context.annotation.Bean;
-import org.testcontainers.containers.MySQLContainer;
 
 @TestConfiguration(proxyBeanMethods = false)
 public class TestMiniDelinaApplication {
 
-    @Bean
-    @ServiceConnection
-    MySQLContainer<?> mysqlContainer() {
-        return new MySQLContainer<>("mysql:latest");
-    }
 
     public static void main(String[] args) {
         SpringApplication.from(MiniDelinaApplication::main).with(TestMiniDelinaApplication.class).run(args);
